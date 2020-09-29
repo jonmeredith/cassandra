f1cd76f58fb7b7db5cce76a25043d862cbd03c75
Jon Meredith <jmeredithco@apple.com>
2020-09-29 16:46:36 -0600

    in-jvm dtests: interpret strings for non-string fields as YAML fragments

--- test/distributed/org/apache/cassandra/distributed/impl/AbstractCluster.java
+++ test/distributed/org/apache/cassandra/distributed/impl/AbstractCluster.java
@@ -32,6 +32,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
