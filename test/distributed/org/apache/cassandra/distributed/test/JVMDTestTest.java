cc817378178a50c1e5cc5137861ac528c9f6468b
Jon Meredith <jmeredithco@apple.com>
2020-10-06 18:00:04 -0600

    add test for schemaChangeIgnoringStoppedInstances

--- test/distributed/org/apache/cassandra/distributed/test/JVMDTestTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/JVMDTestTest.java
@@ -19,6 +19,8 @@
 package org.apache.cassandra.distributed.test;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,8 +28,13 @@ import org.junit.Test;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
+import org.apache.cassandra.distributed.api.Feature;
+import org.apache.cassandra.distributed.api.IInstanceConfig;
+import org.apache.cassandra.gms.Gossiper;
+import org.apache.cassandra.locator.SimpleSeedProvider;
 import org.apache.cassandra.utils.FBUtilities;
 
+import static org.apache.cassandra.distributed.shared.AssertUtils.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -67,6 +74,56 @@ public class JVMDTestTest extends TestBaseImpl
                 Assert.assertEquals(newKeystore, DatabaseDescriptor.getServerEncryptionOptions().keystore);
                 Assert.assertEquals(newTruststore, DatabaseDescriptor.getServerEncryptionOptions().truststore);
             });
+        }
+    }
+
+    @Test
+    public void modifySchemaWithStoppedNode() throws Throwable
+    {
+        try (Cluster cluster = init(Cluster.build().withNodes(2).withConfig(c -> c.with(Feature.GOSSIP).with(Feature.NETWORK)).start()))
+        {
+            Assert.assertFalse(cluster.get(1).isShutdown());
+            Assert.assertFalse(cluster.get(2).isShutdown());
+            cluster.schemaChangeIgnoringStoppedInstances("CREATE TABLE "+KEYSPACE+".tbl1 (id int primary key, i int)");
+
+            cluster.get(2).shutdown(true).get(1, TimeUnit.MINUTES);
+            Assert.assertFalse(cluster.get(1).isShutdown());
+            Assert.assertTrue(cluster.get(2).isShutdown());
+            cluster.schemaChangeIgnoringStoppedInstances("CREATE TABLE "+KEYSPACE+".tbl2 (id int primary key, i int)");
+
+            cluster.get(1).shutdown(true).get(1, TimeUnit.MINUTES);
+            Assert.assertTrue(cluster.get(1).isShutdown());
+            Assert.assertTrue(cluster.get(2).isShutdown());
+
+            // both nodes down, nothing to record a schema change so should get an exception
+            Throwable thrown = null;
+            try
+            {
+                cluster.schemaChangeIgnoringStoppedInstances("CREATE TABLE " + KEYSPACE + ".tblX (id int primary key, i int)");
+            }
+            catch (Throwable tr)
+            {
+                thrown = tr;
+            }
+            Assert.assertNotNull("Expected to fail with all nodes down", thrown);
+
+            // Have to restart instance1 before instance2 as it is hard-coded as the seed in in-JVM configuration.
+            cluster.get(1).startup();
+            cluster.get(2).startup();
+            Assert.assertFalse(cluster.get(1).isShutdown());
+            Assert.assertFalse(cluster.get(2).isShutdown());
+            cluster.schemaChangeIgnoringStoppedInstances("CREATE TABLE "+KEYSPACE+".tbl3 (id int primary key, i int)");
+
+            assertRows(cluster.get(1).executeInternal("SELECT table_name FROM system_schema.tables WHERE keyspace_name = ?", KEYSPACE),
+                       row("tbl1"), row("tbl2"), row("tbl3"));
+            assertRows(cluster.get(2).executeInternal("SELECT table_name FROM system_schema.tables WHERE keyspace_name = ?", KEYSPACE),
+                       row("tbl1"), row("tbl2"), row("tbl3"));
+
+            // Finally test schema can be changed with the first node down
+            cluster.get(1).shutdown(true).get(1, TimeUnit.MINUTES);
+            cluster.schemaChangeIgnoringStoppedInstances("CREATE TABLE "+KEYSPACE+".tbl4 (id int primary key, i int)");
+            assertRows(cluster.get(2).executeInternal("SELECT table_name FROM system_schema.tables WHERE keyspace_name = ?", KEYSPACE),
+                       row("tbl1"), row("tbl2"), row("tbl3"), row("tbl4"));
         }
     }
 }
